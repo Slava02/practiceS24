d4efa772e843a28559602dc49707e7eac05b39df
Slava02 <vazhuvaga@gmail.com>
2024-06-07 14:26:24 +0300

    Implemented dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func showUniverse(w http.ResponseWriter, r *http.Request) {
+func (app *application) showUniverse(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -17,7 +17,7 @@ func showUniverse(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Отображение вселенной c id = %d", id)
 }
 
-func createUniverse(w http.ResponseWriter, r *http.Request) {
+func (app *application) createUniverse(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method is not allowed", 405)
@@ -25,7 +25,7 @@ func createUniverse(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Форма для создания новой вселенной")
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()
@@ -14,15 +19,26 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/universe", showUniverse)
-	mux.HandleFunc("/universe/create", createUniverse)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/universe", app.showUniverse)
+	mux.HandleFunc("/universe/create", app.createUniverse)
 
 	fs := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fs))
 
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Запуск сервера на %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
